8c5cdcc5e65294199ae920f8f5115de217c2b61a
하규호 <hagyuho@hagyuhoui-noteubug.local>
2023-03-07 22:39:58 +0900

    [feat] 양과언덕  언덕 구현

---
+++ work-with-canvas/example1/hill.js
@@ -0,0 +1,81 @@
+export class Hill {
+    constructor(color, speed, total) {
+        this.color = color;
+        this.speed = speed;
+        this.total = total;
+    }
+
+    resize(stageWidth, stageHeight) {
+        this.stageWidth = stageWidth;
+        this.stageHeight = stageHeight;
+
+        this.points = []
+        this.gap = Math.ceil(this.stageWidth / (this.total - 2)) // 간격을 조금 넓게 정의
+
+        for (let i = 0; i < this.total; i++) {
+            this.points[i] = {
+                x: i * this.gap,
+                y: this.getY()
+            }
+
+        }
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+
+        let cur = this.points[0];
+        let prev = cur;
+
+        let dots = [];
+        ctx.x += this.speed; // hill이 움직이게 함
+        ctx.moveTo(cur.x, cur.y);
+
+        if (cur.x > this.gap) { // 끊임없는 언덕 만들어주기
+            this.points.unshift({
+                x: -(this.gap * 2),
+                y: this.getY()
+            })
+        } else if (cur.x > this.stageWidth + this.gap) {
+            this.points.splice(-1)
+        }
+
+        let prevCx = cur.x;
+        let prevCy = cur.y;
+
+        for (let i = 1; i < this.points.length; i++) {
+            cur = this.points[i];
+            cur.x += this.speed; //hill이 움직이게 함
+            const cx = (prev.x + cur.x) / 2;
+            const cy = (prev.x + cur.y) / 2;
+            ctx.quadraticCurveTo(prev.x, prev.y, cx, cy);
+
+            dots.push({
+                x1: prevCx,
+                y1: prevCy,
+                x2: prev.x, // ?? x1, x2 같은거 아님??
+                y2: prev.y,
+                x3: cx,
+                y3: cy,
+            });
+
+            prev = cur;
+            prevCx = cx;
+            prevCy = cy;
+        }
+
+        ctx.lineTo(prev.x, prev.y);
+        ctx.lineTo(this.stageWidth, this.stageHeight);
+        ctx.lineTo(this.points[0].x, this.stageHeight);
+        ctx.fill()
+
+        return dots;
+    }
+
+    getY() {
+        const min = this.stageHeight / 8;
+        const max = this.stageHeight - min;
+        return min + Math.random() * max;
+    }
+}
