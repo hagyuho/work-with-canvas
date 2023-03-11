57cb29919b503025ac59420feb629269212bd99e
hagyuho <hagyuho90@gmail.com>
2023-03-11 22:17:48 +0900

    [feat] 양과늑대 구현 완료

--- work-with-canvas/example1/app.js
+++ work-with-canvas/example1/app.js
@@ -1,15 +1,22 @@
 import { Hill } from "./hill.js";
+
+import { SheepController } from './sheep-controller.js'
+import {Sun} from './sun.js'
 class App {
-    constructor(){
+    constructor() {
         this.canvas = document.createElement('canvas')
         this.ctx = this.canvas.getContext('2d')
         document.body.appendChild(this.canvas)
-    
+
+        this.sun = new Sun();
+
         this.hills = [
             new Hill('#ff6bea', 0.2, 12),
             new Hill('#ff59c2', 0.5, 8),
             new Hill('#ff4674', 1.4, 6)
         ]
+
+        this.SheepController = new SheepController()
 
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize()
@@ -17,28 +24,36 @@ class App {
         requestAnimationFrame(this.animate.bind(this));
     }
 
-    resize(){
+    resize() {
         this.stageWidth = document.body.clientWidth;
         this.stageHeight = document.body.clientHeight;
 
-        this.canvas.width = this.stageWidth * 1.5;
-        this.canvas.height = this.stageHeight * 1.5;
-        this.ctx.scale(1.5,1.5);
+        this.canvas.width = this.stageWidth * 2;
+        this.canvas.height = this.stageHeight * 2;
+        this.ctx.scale(2, 2);
+
+        this.sun.resize(this.stageWidth,this.stageHeight);
 
         for (let i = 0; i < this.hills.length; i++) {
-            this.hills[i].resize(this.stageWidth,this.stageHeight);            
+            this.hills[i].resize(this.stageWidth, this.stageHeight);
         }
+
+        this.SheepController.resize(this.stageWidth, this.stageHeight)
     }
 
-    animate(t){
+    animate(t) {
         requestAnimationFrame(this.animate.bind(this));
 
-        this.ctx.clearRect(0,0,this.stageWidth, this.stageHeight)
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight)
+
+        this.sun.draw(this.ctx,t);
 
         let dots;
         for (let i = 0; i < this.hills.length; i++) {
-            dots = this.hills[i].draw(this.ctx);            
+            dots = this.hills[i].draw(this.ctx);
         }
+
+        this.SheepController.draw(this.ctx, t, dots) // t: timestamp, FPS 정의 가능
     }
 }
 
